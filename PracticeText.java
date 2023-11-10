578746c61bbfb5636dafa0d269e460ed5d956f34
Ankit Patel <ankitpatel1984@gmail.com>
2023-11-10 17:36:24 +0000

    JavaHomeworkUpdated

--- PracticeText.java
+++ PracticeText.java
@@ -1,12 +1,17 @@
 package javahomework2;
 
-public class PracticeClass {
+public class PracticeText {
     public static void main(String[] args) {
         int number=199;
-        if (number>0){
+        if (number<0){
             System.out.println("The Statement is Positive");
-        }
+        }else
+        {
+            System.out.println("The Statement is Negative");
+
         }
 
 
     }
+}
+
